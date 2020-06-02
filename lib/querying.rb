819ec2149dc1242aa1dc939c5878c4aac565c77f
Uriah Harston <uharston@gmail.com>
2020-06-02 15:08:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.years
-  FROM books 
+  FROM books
   WHERE books.series_id = 1
-  ORDER BY books.years 
+  ORDER BY books.years
  "
 end
 
