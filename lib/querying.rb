e2f3aeebe84c4c00b24d9a0fe5007a1ff21af9a3
Uriah Harston <uharston@gmail.com>
2020-06-02 15:27:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,10 +31,12 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
-JOIN characters ON series.id = characters.series_id
-WHERE characters.species = 'human' LIMIT 1"
+  JOIN characters ON series.id = characters.series_id
+  WHERE characters.species = 'human' LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.character_id) FROM characters 
+  JOIN character_books ON characters.id = character_books.character_id
+  GROUP BY characters.name ORDER BY COUNT(characters.id) DESC"
 end
