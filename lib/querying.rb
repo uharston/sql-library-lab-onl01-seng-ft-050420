41fc425db81e17d1f3f352ce659ef14e18a0ae04
Uriah Harston <uharston@gmail.com>
2020-06-02 15:44:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -50,8 +50,8 @@ end
 
 
 
-
-
-"SELECT characters.name, COUNT(character_books.character_id) FROM characters
-JOIN character_books ON characters.id = character_books.character_id
-GROUP BY characters.name ORDER BY COUNT(characters.id) DESC"
+# 
+# 
+# "SELECT characters.name, COUNT(character_books.character_id) FROM characters
+# JOIN character_books ON characters.id = character_books.character_id
+# GROUP BY characters.name ORDER BY COUNT(characters.id) DESC"
