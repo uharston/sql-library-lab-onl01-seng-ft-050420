817eb38d4ab4924daae5aa2486a7aa454eeb13a0
Uriah Harston <uharston@gmail.com>
2020-06-02 15:18:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,8 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
   FROM characters
-  GROUP BY species  "
+  GROUP BY species
+  ORDER BY DESC  "
 end
 
 def select_name_and_series_subgenres_of_authors
