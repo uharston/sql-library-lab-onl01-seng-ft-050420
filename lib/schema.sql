bbb78bd75d92be68e44c6f3e33365d6f18086523
Uriah Harston <uharston@gmail.com>
2020-06-02 14:59:23 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -13,23 +13,23 @@ CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
   name TEXT);
 
--- CREATE TABLE books (
---   id INTEGER PRIMARY KEY,
---   title TEXT,
---   year INTEGER,
---   series_id INTEGER
--- );
---
--- CREATE TABLE characters (
---   id INTEGER PRIMARY KEY,
---   name TEXT,
---   motto TEXT,
---   species TEXT,
---   author_id = INTEGER
--- );
---
--- CREATE TABLE character_books (
---   id INTEGER PRIMARY KEY,
---   character_id INTEGER,
---   book_id INTEGER
--- );
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id = INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER
+);
