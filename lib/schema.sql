8dbeb99cbfd568e72a7bc4fae0524ba584f0d92a
Uriah Harston <uharston@gmail.com>
2020-06-02 14:58:16 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -9,11 +9,11 @@ CREATE TABLE subgenres (
   name INTEGER
 );
 
--- CREATE TABLE author (
---   id INTEGER PRIMARY KEY,
---   name TEXT
--- );
---
+CREATE TABLE author (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
 -- CREATE TABLE books (
 --   id INTEGER PRIMARY KEY,
 --   title TEXT,
