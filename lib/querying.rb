54f113aeecd34ace5aead5d6d8993cf03b29239e
Uriah Harston <uharston@gmail.com>
2020-06-02 15:37:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
+  "SELECT DISTINCT series.title
   FROM series
   JOIN authors ON series.author_id = authors.id
   JOIN characters ON authors.id = characters.author_id
