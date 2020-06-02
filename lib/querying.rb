c09c991a09cdf2bab443f419f7d62d73278d3067
Uriah Harston <uharston@gmail.com>
2020-06-02 15:27:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,8 +30,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series 
-JOIN characters ON series.id = characters.series_id 
+  "SELECT series.title FROM series
+JOIN characters ON series.id = characters.series_id
 WHERE characters.species = 'human' LIMIT 1"
 end
 
