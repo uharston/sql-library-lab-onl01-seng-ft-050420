2a492764813f293708753dfbfa81724467381765
Uriah Harston <uharston@gmail.com>
2020-06-02 15:25:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM author
+  FROM authors
   JOIN series ON authors.id = series.author_id
   JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
